9e3a96609238e9e0134238917e95e52344712817
YoshikiHigo <yoshiki.higo@gmail.com>
2018-09-23 13:18:14 +0900

    add logs for execution time

--- src/main/java/jp/kusumotolab/finergit/FinerRepo.java
+++ src/main/java/jp/kusumotolab/finergit/FinerRepo.java
@@ -9,7 +9,6 @@ import org.eclipse.jgit.api.CommitCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.MergeCommand;
 import org.eclipse.jgit.api.MergeCommand.FastForwardMode;
-import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.MergeResult.MergeStatus;
 import org.eclipse.jgit.api.RmCommand;
 import org.eclipse.jgit.api.Status;
@@ -21,6 +20,7 @@ import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.merge.MergeStrategy;
 import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.submodule.SubmoduleWalk.IgnoreSubmoduleMode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import jp.kusumotolab.finergit.util.RevCommitUtil;
@@ -29,13 +29,25 @@ public class FinerRepo {
 
   private static final Logger log = LoggerFactory.getLogger(FinerRepo.class);
 
-  public final Path path;
   private Git git;
+  public final Path path;
+  private final Timer addStopWatch;
+  private final Timer checkoutStopWatch;
+  private final Timer commitStopWatch;
+  private final Timer mergeStopWatch;
+  private final Timer rmStopWatch;
+  private final Timer statusStopWatch;
 
   public FinerRepo(final Path path) {
     log.trace("enter FinerRepo(Path=\"{}\")", path.toString());
-    this.path = path;
     this.git = null;
+    this.path = path;
+    this.addStopWatch = new Timer();
+    this.checkoutStopWatch = new Timer();
+    this.commitStopWatch = new Timer();
+    this.mergeStopWatch = new Timer();
+    this.rmStopWatch = new Timer();
+    this.statusStopWatch = new Timer();
   }
 
   public boolean initialize() {
@@ -73,21 +85,27 @@ public class FinerRepo {
         "enter doCheckoutCommand(String=\"{}\", boolean=\"{}\", RevCommit=\"{}\", boolean=\"{}\")",
         branchName, create, RevCommitUtil.getAbbreviatedID(startPoint), orphan);
 
-    final CheckoutCommand checkoutCommand = this.git.checkout();
-    checkoutCommand.setCreateBranch(create)
+    this.checkoutStopWatch.start();
+    final CheckoutCommand checkoutCommand = this.git.checkout()
+        .setCreateBranch(create)
         .setName(branchName)
         .setStartPoint(startPoint)
         .setOrphan(orphan);
+    boolean success = true;
+
     try {
       checkoutCommand.call();
-      return true;
     } catch (final Exception e) {
+      success = false;
       log.error(
           "git-checkout command failed, branchName <{}>, create <{}>, startPoint <{}>, orphan <{}>",
           branchName, create, RevCommitUtil.getAbbreviatedID(startPoint), orphan);
       log.error(e.getMessage());
-      return false;
+    } finally {
+      this.checkoutStopWatch.suspend();
     }
+
+    return success;
   }
 
   public boolean doAddCommand(final Collection<String> paths) {
@@ -98,18 +116,23 @@ public class FinerRepo {
       return false;
     }
 
+    this.addStopWatch.start();
     final AddCommand addCommand = this.git.add()
         .setUpdate(false);
     paths.forEach(addCommand::addFilepattern);
+    boolean success = true;
 
     try {
       addCommand.call();
-      return true;
     } catch (final Exception e) {
+      success = false;
       log.error("git-add command failed");
       log.error(e.getMessage());
-      return false;
+    } finally {
+      this.addStopWatch.suspend();
     }
+
+    return success;
   }
 
   public boolean doRmCommand(final Collection<String> paths) {
@@ -120,17 +143,23 @@ public class FinerRepo {
       return false;
     }
 
+    this.rmStopWatch.start();
     final RmCommand rmCommand = this.git.rm();
     paths.stream()
         .forEach(rmCommand::addFilepattern);
+    boolean success = true;
+
     try {
       rmCommand.call();
-      return true;
     } catch (final Exception e) {
+      success = false;
       log.error("git-rm command failed");
       log.error(e.getMessage());
-      return false;
+    } finally {
+      this.rmStopWatch.suspend();
     }
+
+    return success;
   }
 
   public RevCommit doCommitCommand(final PersonIdent personIdent, final String originalCommitID,
@@ -138,57 +167,70 @@ public class FinerRepo {
     log.trace("enter doCommitCommand(PersonIdent=\"{}\", String=\"{}\", String=\"{}\")",
         personIdent.toExternalString(), originalCommitID, originalCommitMessage);
 
+    this.commitStopWatch.start();
     final CommitCommand commitCommand = this.git.commit();
     final String message = "<OriginalCommitID:" + originalCommitID + "> " + originalCommitMessage;
+    RevCommit commit = null;
     try {
-      final RevCommit commit = commitCommand.setAuthor(personIdent)
+      commit = commitCommand.setAuthor(personIdent)
           .setMessage(message)
           .call();
-      return commit;
     } catch (final Exception e) {
       log.error(
           "git-commit command failed, personIdent<{}>, originalCommitID<{}>, originalCommitMessage<{}>",
           personIdent.toExternalString(), originalCommitID, originalCommitMessage);
       log.error(e.getMessage());
-      return null;
+    } finally {
+      this.commitStopWatch.suspend();
     }
+
+    return commit;
   }
 
   public MergeStatus doMergeCommand(final RevCommit targetCommit) {
     log.trace("enter doMergeCommand(RevCommit=\"{}\")",
         RevCommitUtil.getAbbreviatedID(targetCommit));
 
+    this.mergeStopWatch.start();
     final MergeCommand mergeCommit = this.git.merge();
+    MergeStatus mergeStatus = null;
     try {
-      final MergeResult mergeResult = mergeCommit.include(targetCommit)
+      mergeStatus = mergeCommit.include(targetCommit)
           .setCommit(false)
           .setFastForward(FastForwardMode.NO_FF)
           .setStrategy(MergeStrategy.OURS)
-          .call();
-      return mergeResult.getMergeStatus();
+          .call()
+          .getMergeStatus();
     } catch (final GitAPIException e) {
       log.error("git-merge command failed due to GitAPIException");
       log.error(e.getMessage());
-      return null;
     } catch (final Exception e) {
       log.error("git-merge command failed to unknown reason");
       log.error(e.getMessage());
-      return null;
+    } finally {
+      this.mergeStopWatch.suspend();
     }
+
+    return mergeStatus;
   }
 
   public Status doStatusCommand() {
     log.trace("enter doStatusCommand()");
 
-    final StatusCommand statusCommand = this.git.status();
+    this.statusStopWatch.start();
+    final StatusCommand statusCommand = this.git.status()
+        .setIgnoreSubmodules(IgnoreSubmoduleMode.ALL);
+    Status status = null;
     try {
-      final Status status = statusCommand.call();
-      return status;
+      status = statusCommand.call();
     } catch (final NoWorkTreeException | GitAPIException e) {
       log.error("git-status command failed");
       log.error(e.getMessage());
-      return null;
+    } finally {
+      this.statusStopWatch.suspend();
     }
+
+    return status;
   }
 
   public String getCurrentBranch() {
@@ -199,5 +241,29 @@ public class FinerRepo {
       log.error("failed to access repository \"{}\"", this.path);
       return "";
     }
+  }
+
+  public String getAddCommandExecutionTime() {
+    return this.addStopWatch.toString();
+  }
+
+  public String getCheckoutCommandExcecutionTime() {
+    return this.checkoutStopWatch.toString();
+  }
+
+  public String getCommitCommandExecutionTime() {
+    return this.commitStopWatch.toString();
+  }
+
+  public String getMergeCommandExecutionTime() {
+    return this.mergeStopWatch.toString();
+  }
+
+  public String getRmCommandExecutionTime() {
+    return this.rmStopWatch.toString();
+  }
+
+  public String getStatusCommandExcutionTime() {
+    return this.statusStopWatch.toString();
   }
 }
