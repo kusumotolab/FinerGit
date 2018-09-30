30758b3c0c63e1246654797878580f6f4d7e4803
YoshikiHigo <yoshiki.higo@gmail.com>
2018-10-01 07:31:18 +0900

    add new test cases

---
+++ src/test/resources/finergit/ast/token/NestedClass.java
@@ -0,0 +1,28 @@
+package finergit.ast.token;
+
+
+public class NestedClass {
+
+  // 抽出されないはず
+  class InnerClass01 {
+  }
+
+  // 抽出されるはず
+  void method01() {
+
+    // 抽出されないはず
+    new Runnable() {
+
+      @Override
+      public void run() {}
+    };
+  }
+
+  // 抽出されるはず
+  void method02() {
+
+    // 抽出されないはず
+    class InnerClass02 {
+    }
+  }
+}
