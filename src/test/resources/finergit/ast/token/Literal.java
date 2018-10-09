7acd2efe49793df9ffa33e95929cc0efdcd8455e
YoshikiHigo <yoshiki.higo@gmail.com>
2018-10-09 22:28:02 +0900

    add a test resource for literals

---
+++ src/test/resources/finergit/ast/token/Literal.java
@@ -0,0 +1,13 @@
+package finergit.ast.token;
+
+
+public class Literal {
+
+  void method01() {
+    System.out.println(0);
+    System.out.println(0l);
+    System.out.println(0f);
+    System.out.println("0");
+    System.out.println('0');
+  }
+}
