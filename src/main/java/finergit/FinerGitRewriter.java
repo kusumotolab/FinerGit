cbc5a871ac0a1db113402d9da4816657a7388504
Shinpei Hayashi <hayashi@c.titech.ac.jp>
2018-12-17 11:07:47 +0900

    Log the association of source module and generated finer modules

--- src/main/java/finergit/FinerGitRewriter.java
+++ src/main/java/finergit/FinerGitRewriter.java
@@ -2,10 +2,10 @@ package finergit;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.revwalk.RevCommit;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import finergit.ast.FinerJavaFileBuilder;
 import finergit.ast.FinerJavaModule;
 import finergit.util.RevCommitUtil;
@@ -15,6 +15,7 @@ import jp.ac.titech.c.se.stein.core.EntrySet.Entry;
 import jp.ac.titech.c.se.stein.core.EntrySet.EntryList;
 
 public class FinerGitRewriter extends ConcurrentRepositoryRewriter {
+  private static final Logger log = LoggerFactory.getLogger(FinerGitRewriter.class);
 
   private final FinerGitConfig config;
 
@@ -46,12 +47,14 @@ public class FinerGitRewriter extends ConcurrentRepositoryRewriter {
     // Convert to finer modules
     final EntryList result = new EntryList();
     if (config.isOriginalJavaIncluded()) {
+      log.debug("Keep original file: {}", entry);
       result.add(entry);
     }
     for (final FinerJavaModule m : extractFinerModules(entry)) {
       final String finerSource = String.join(System.lineSeparator(), m.getLines());
       final ObjectId newId = writeBlob(finerSource.getBytes(StandardCharsets.UTF_8));
       final String name = m.getFileName();
+      log.debug("Generate finer module: {} -> {} {}", entry, name, newId.name());
       result.add(new Entry(entry.mode, name, newId, entry.pathContext));
     }
     return result;
