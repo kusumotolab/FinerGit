a9fa78829f622ce7a0e455e68307679edc2ca260
Yoshiki Higo <yoshiki.higo@gmail.com>
2019-03-26 17:59:32 +0900

    bug fix

--- src/main/java/finergit/ast/token/STRINGLITERAL.java
+++ src/main/java/finergit/ast/token/STRINGLITERAL.java
@@ -3,6 +3,6 @@ package finergit.ast.token;
 public class STRINGLITERAL extends JavaToken {
 
   public STRINGLITERAL(final String value) {
-    super("\"" + value + "\"");
+    super(value);
   }
 }
