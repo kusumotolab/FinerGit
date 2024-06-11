5138cfb5ff43e23f177183ca52dc8635f317bf77
Yoshiki Higo <yoshiki.higo@gmail.com>
2024-06-11 12:08:28 +0900

    remove unused import

--- src/main/java/finergit/ast/JavaFileVisitorExecutor.java
+++ src/main/java/finergit/ast/JavaFileVisitorExecutor.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.google.common.base.Charsets;
